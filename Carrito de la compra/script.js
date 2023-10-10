8fc2b60cde2ef6a7d74cb8f04a88ae9f68dacc8b
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-10-10 12:40:17 +0200

    Opciones de pago e imprimir completadas

--- Carrito de la compra/script.js
+++ Carrito de la compra/script.js
@@ -8,17 +8,51 @@ let articulosCarrito;
 let precioTotal;
 let formaPago;
 
+//variables para almacenar datos
+let articulo;
+let precio;
+let cantidad;
+
+//acumuladores
+let acumuladorPrecio;
+let total;
+let precioAnterior;
+
+//mensaje errores
+let mensajeErrorNombre;
+let mensajeErrorPrecio;
+
+//forma de pago
+
+let cajasTarjeta;
+let cajaEfectivo;
+let stringFormaPago;
 
 
 //inicializar variables en funcion
 function initVariables(){
     arrayArticulos = [];
     //arrayArticulos = new Array();
-    nombreArticulo=document.getElementById("nombre-articulo");
-    precioArticulo=document.getElementById("precio-articulo");
+    
+    nombreArticulo = document.getElementById("nombre-articulo");
+    nombreArticulo.focus();
+    precioArticulo = document.getElementById("precio-articulo");
     unidades=document.getElementById("unidades");
     botonAñadir=document.getElementById("añadir-carrito");
-
+    articulosCarrito = document.getElementById("total-articulos");
+    precioTotal = document.getElementById("precio-total");
+    precioTotal.value='0';
+    mensajeErrorNombre = document.getElementById("error-nombre");
+    mensajeErrorPrecio = document.getElementById("error-precio");
+    formaPago = document.getElementById("pago");
+    formaPago.value='';
+    cajasTarjeta = document.getElementById("tarjeta-credito");
+    cajaEfectivo =  document.getElementById("efectivo");
+    cajasTarjeta.style.display='none';
+    cajaEfectivo.style.display='none';
+    //mensajeErrorNombre.style.border = 'white';
+    //mensajeErrorNombre.style.display = 'none';
+    document.getElementById('imprimir').disabled = true;
 }
 
 //funciones
@@ -29,33 +63,116 @@ function nombreFuncion(){
 //funcion añadir articulos al array
 function añadirArticulos(){
 
-    nombreArray.push(elementos);
+    articulo = nombreArticulo.value;
+    //precio por pantalla
+    precio= parseFloat(precioArticulo.value);
+    cantidad = parseFloat(unidades.value);
+    total = precio * cantidad;
+    //le sumo la cantidad que habia en la caja antes al actual para acumular
+    precioAnterior = parseFloat(precioTotal.value);
+    acumuladorPrecio = precioAnterior + total;
+    precioTotal.value = acumuladorPrecio.toString();
+
+    //meter el articulo en el array
+    arrayArticulos.push(articulo);
     //nombreArray.unshift(elementos) Añade el elemento del array al principio
+    articulosCarrito.value = arrayArticulos;
+    resetCajas();
+
 }
+
+//funcion reset las cajas de texto
+ function resetCajas(){
+    nombreArticulo.value='';
+    precioArticulo.value='';
+    unidades.value=1;
+    nombreArticulo.focus();
+ }
 
 //Onload con los listeners
 
 window.addEventListener("load",()=>{
+    //inicializo variables
+    initVariables();
 
-    
+    //listener añadir al carrito con errores si estan las cajas vacias
+    botonAñadir.addEventListener('click', () => {
 
-	//listener empezar partida al darle a aceptar
-	botonAñadir.addEventListener('click', () => {
+        if(nombreArticulo.value!='' && precioArticulo.value !=''){
+            mensajeErrorNombre.textContent = '';
+            mensajeErrorPrecio.textContent = '';
+            añadirArticulos();
+        }
+        else{
+            if(nombreArticulo.value=='' && precioArticulo.value==''){
+                mensajeErrorNombre.textContent = 'falta nombre';
+                mensajeErrorPrecio.textContent = 'falta precio';
+                nombreArticulo.focus();
+            }else if(precioArticulo.value==''){
+                mensajeErrorPrecio.textContent = 'falta precio';
+                mensajeErrorNombre.textContent = '';
+                precioArticulo.focus();
+            }else if(nombreArticulo.value==''){
+                mensajeErrorNombre.textContent = 'falta nombre';
+                mensajeErrorPrecio.textContent = '';
+                nombreArticulo.focus();
+            }
 
-	
-		
+        }
+        	
+	});
+    //listener presionar forma de pago aparecen las opciones correspondientes de efectivo o tarjeta
+    formaPago.addEventListener('click', () => {
+        if(formaPago.value == 'TAR'){
+            cajasTarjeta.style.display= 'inline';
+            cajaEfectivo.style.display='none';
+            stringFormaPago="tarjeta";
+        }
+        if(formaPago.value == 'EF'){
+            cajaEfectivo.style.display='inline';
+            cajasTarjeta.style.display= 'none';
+            document.getElementById('importe-efectivo').value= precioTotal.value;
+            stringFormaPago = "efectivo";
+        }
+      
+        	
 	});
 
-    nombreCaja.addEventListener("input",()=>{
-
+    //listener al darle al checkbox se habilita el boton imprimir
+    document.getElementById('aceptar').addEventListener('click', () => {
+        
+        document.getElementById('imprimir').disabled = false;
+        	
+	});
+    //listener para resumen resultados al hacer click sobre la puntuacion
+	document.getElementById('imprimir').addEventListener("click",()=>{
 		
-		
+        if(stringFormaPago == "efectivo" || stringFormaPago == "tarjeta"){
+            alert('Los articulos de mi carrito son: '+ arrayArticulos + '\n'+
+			'y el precio total es de: '+ precioTotal.value + ' €'  +'\n'+
+			'forma de pago: ' + stringFormaPago + '\n');
+        }else{
+            alert('Seleccione una forma de pago');
+        } 
+        
 	});
 
-	//listener pata que aparezca un promt si pierdes el foco de la caja de texto sin poner el nombre
-	nombreCaja.addEventListener("blur",()=>{
-	
 
+    document.getElementById('resetear').addEventListener("click",()=>{
+		
+        
+        nombreArticulo.focus();
+        //limpiar el array de objetos tambien
+        arrayArticulos = [];
+        precioTotal.value='0';
+        articulosCarrito.value='';
+        document.getElementById('importe-efectivo').value= '';
+        formaPago.value='';
+
+        //Otra opcion recargando la pagina
+        //window.location.reload()
+ 
 	});
+
 
 });
